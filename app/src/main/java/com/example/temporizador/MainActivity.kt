e3c990d9b6e799b32b3f75f84bf0d32a3563d28c
EdvaldoCariege <osvaldocariege06@gmail.com>
2024-12-18 16:03:10 +0100

    My first project in Kotlin

---
+++ app/src/main/java/com/example/temporizador/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.temporizador
+
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import java.lang.NumberFormatException
+
+class MainActivity : AppCompatActivity() {
+
+    private var timer: CountDownTimer? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+    }
+}
